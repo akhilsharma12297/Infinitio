ae1b4fdcfc15f32a83d2b7225e011a97a9bf047b
Akhil sharma <37407049+akhilsharma12297@users.noreply.github.com>
2018-08-20 16:41:23 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,150 @@
+<?php
+	include("includes/config.php");
+	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
+
+	$account = new Account($con);
+
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
+?>
+
+<html>
+<head>
+	<title>Welcome to INFINITO!</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/register.js"></script>
+</head>
+<body>
+	<?php
+
+	if(isset($_POST['registerButton'])) {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").hide();
+					$("#registerForm").show();
+				});
+			</script>';
+	}
+	else {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").show();
+					$("#registerForm").hide();
+				});
+			</script>';
+	}
+
+	?>
+
+
+	<div id="background">
+
+		<img src="assets\images\title.png"style="height: 200;margin-left: 450;">
+
+		<div id="loginContainer">
+
+			<div id="inputContainer">
+				<form id="loginForm" action="register.php" method="POST">
+					<h2>Login To Your Account</h2>
+					<p>
+						<?php echo $account->getError(Constants::$loginFailed); ?>
+						<label for="loginUsername">Username</label>
+						<input id="loginUsername" name="loginUsername" type="text" placeholder="Enter Your Username" value="<?php getInputValue('loginUsername') ?>" required autocomplete="off">
+					</p>
+					<p>
+						<label for="loginPassword">Password</label>
+						<input id="loginPassword" name="loginPassword" type="password" placeholder="Enter Your Password" required>
+					</p>
+
+					<button type="submit" name="loginButton">LOG IN</button>
+
+					<div class="hasAccountText">
+						<span id="hideLogin">Don't Have An Account Yet? Signup Here.</span>
+					</div>
+
+				</form>
+
+
+
+				<form id="registerForm" action="register.php" method="POST">
+					<h2>Create your free account</h2>
+					<p>
+						<?php echo $account->getError(Constants::$usernameCharacters); ?>
+						<?php echo $account->getError(Constants::$usernameTaken); ?>
+						<label for="username">Username</label>
+						<input id="username" name="username" type="text" placeholder="Enter Your Username" value="<?php getInputValue('username') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$firstNameCharacters); ?>
+						<label for="firstName">First name</label>
+						<input id="firstName" name="firstName" type="text" placeholder="Enter Your First Name" value="<?php getInputValue('firstName') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$lastNameCharacters); ?>
+						<label for="lastName">Last name</label>
+						<input id="lastName" name="lastName" type="text" placeholder="Enter Your Last Name" value="<?php getInputValue('lastName') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+						<?php echo $account->getError(Constants::$emailInvalid); ?>
+						<?php echo $account->getError(Constants::$emailTaken); ?>
+						<label for="email">Email</label>
+						<input id="email" name="email" type="email" placeholder="Enter Your Email Id" value="<?php getInputValue('email') ?>" required>
+					</p>
+
+					<p>
+						<label for="email2">Confirm email</label>
+						<input id="email2" name="email2" type="email" placeholder="Confirm Your Email Id" value="<?php getInputValue('email2') ?>" required>
+					</p>
+
+					<p>
+						<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+						<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+						<?php echo $account->getError(Constants::$passwordCharacters); ?>
+						<label for="password">Password</label>
+						<input id="password" name="password" type="password" placeholder="Enter Your password" required>
+					</p>
+
+					<p>
+						<label for="password2">Confirm password</label>
+						<input id="password2" name="password2" type="password" placeholder="Confirm Your password" required>
+					</p>
+
+					<button type="submit" name="registerButton">SIGN UP</button>
+
+					<div class="hasAccountText">
+						<span id="hideRegister">Already Have An Account? Log In Here.</span>
+					</div>
+
+				</form>
+
+
+			</div>
+
+			<div id="loginText">
+				<h1>Your Protal To INFINTE Music!</h1>
+				<ul>
+					<li>Access To Ultimate Music Of All Kind Of Generus.</li>
+					<li>Create Playlist Of Songs You Love.</li>
+					<li>Follow Artist You Admire.</li>
+				</ul>
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
